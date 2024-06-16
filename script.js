67eac3016845938f9b4eeccd1d666d290050cfd9
Ishan Narayan Shrestha <ishaanstha18@gmail.com>
2024-06-16 14:41:01 +0545

    asking user's choice

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ console.log(choices[randomChoice])
 
 // call the function 
 getComputerChoice()
+
+
+
+// Logic to get human choice
+
+// CREATE a function named getHumanChoice
+function getHumanChoice() {
+
+    // INPUT the choice of user and STORE it in a variable
+    let userChoice = prompt("Enter rock paper or scissors").toLowerCase();
+    
+    // IF the answer == "rock" || "paper" || "scissors" THEN
+    if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors" ) {
+    // DISPLAY answer
+    console.log(`${userChoice}`)
+    }
+    // ELSE
+    else {
+    // DISPLAY "ERROR"
+    console.log("ERROR")
+        }
+    }
+    // CALL function
+    getHumanChoice()
