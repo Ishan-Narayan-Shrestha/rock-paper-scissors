5a24bbde4c1211916d7365889f001fc6e0d2c10c
Ishan Narayan Shrestha <ishaanstha18@gmail.com>
2024-06-16 14:39:45 +0545

    asking computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World!")
+
+// Logic to get computer choice
+
+// create a function name called getComputerChoice
+function getComputerChoice() {
+
+// create a variable which store rock, paper, scissors.
+let choices = ["rock", "paper", "scissors"];
+
+// create a variable which will randomly select and STORE the data from previous variable.
+let randomChoice = Math.floor(Math.random() * choices.length);
+
+// DISPLAY the output of randomly chosen data
+console.log(choices[randomChoice])
+}
+
+// call the function 
+getComputerChoice()
